c0931fc307157b4ddd74414fdc41ad03ef63c5c1
SongMc <1471485017@qq.com>
2021-04-11 23:18:00 +0800

    test0111111

---
+++ src/main/java/com/atguigu/jxc/util/test.java
@@ -0,0 +1,29 @@
+package com.atguigu.jxc.util;
+
+/**
+ * @author : SongMc
+ * @date : 2021/4/11 22:22
+ * className : test
+ * package: com.atguigu.jxc.util
+ * version : 1.0
+ * Description
+ */
+public class test {
+
+    public static void main(String args[]){
+        System.out.println("test01111");
+        System.out.println("test01111");
+        System.out.println("test01111");
+        System.out.println("test01111");
+        System.out.println("test01111");
+        System.out.println("test01111");
+        System.out.println("test01111");
+        System.out.println("test01111");
+        System.out.println("test01111");
+        System.out.println("test01111");
+        System.out.println("test01111");
+        System.out.println("test01111");
+        System.out.println("test01111");
+        System.out.println("test01111");
+    }
+}
